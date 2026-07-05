d5b76de8ddca05f44a604a4986da9f07983ad850
khianvictorycalderon <ckhianvictory@gmail.com>
2026-07-05 16:48:11 +0800

    Added CORS

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,23 @@
 // Builder
 var builder = WebApplication.CreateBuilder(args);
 
+// Services
+builder.Services.AddCors(options =>
+{
+   options.AddDefaultPolicy(policy =>
+   {
+      policy
+        .WithOrigins(builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>() ?? Array.Empty<string>())
+        .AllowAnyHeader()
+        .WithMethods("GET");
+   });
+});
+
 // Main Application
 var app = builder.Build();
+
+// Middlewares
+app.UseCors();
 
 // Application Runner
 app.Run();
