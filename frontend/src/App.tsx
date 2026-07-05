7bd7f95b802823ac5b349fd4c734b4d71ba748c9
khianvictorycalderon <ckhianvictory@gmail.com>
2026-07-05 16:34:30 +0800

    Added card layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import Card from "./components/Card";
+
 export default function App() {
   return (
     <div className="w-full p-8">
       
+      <Card
+        title="safsafsaf"
+        description="asfsafs"
+      />
+
     </div>
   );
 }
--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,7 +1,16 @@
-const Card = () => {
+interface CardProps {
+    title: string;
+    description: string;
+}
+
+const Card = ({
+    title,
+    description
+}: CardProps) => {
   return (
-    <div>
-      
+    <div className="flex flex-col gap-2">
+      <h2>{title}</h2>
+      <p>{description}</p>
     </div>
   );
 }
